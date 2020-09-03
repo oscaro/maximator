546d9d3959bf5056faf0144ec2010df9ee1bcd84
Blazej Pindelski <blazej.pindelski@bloomdiagnostics.com>
2020-09-03 13:58:02 +0200

    feat: Update MaxMind client to 2.14.0

--- src/com/oscaro/maximator.clj
+++ src/com/oscaro/maximator.clj
@@ -7,9 +7,11 @@
            [com.maxmind.db Reader$FileMode]
            [java.net InetAddress]))
 
+
 (def ^:no-doc file-modes
   {:memory-mapped (Reader$FileMode/valueOf "MEMORY_MAPPED")
-   :memory (Reader$FileMode/valueOf "MEMORY")})
+   :memory        (Reader$FileMode/valueOf "MEMORY")})
+
 
 (defn- ^:no-doc names-as-kw
   [^AbstractNamedRecord o]
@@ -19,37 +21,51 @@
       (assoc! acc (keyword k) v))
     (transient {}) (.getNames o))))
 
+
 (def ^:no-doc translator
   (g/register-converters
-    {:exclude [:class]}
-    [["com.maxmind.geoip2.model.CityResponse"]
-     ["com.maxmind.geoip2.model.ConnectionTypeResponse"]
-     ["com.maxmind.geoip2.model.IspResponse"]
-     ["com.maxmind.geoip2.model.DomainResponse"]
-     ["com.maxmind.geoip2.record.City" :exclude [:names] :add {:names names-as-kw}]
-     ["com.maxmind.geoip2.record.Continent" :exclude [:names] :add {:names names-as-kw}]
-     ["com.maxmind.geoip2.record.Subdivision" :exclude [:names] :add {:names names-as-kw}]
-     ["com.maxmind.geoip2.record.RepresentedCountry" :exclude [:names] :add {:names names-as-kw}]
-     ["com.maxmind.geoip2.record.Location"]
-     ["com.maxmind.geoip2.record.MaxMind"]
-     ["com.maxmind.geoip2.record.Postal"]
-     ["com.maxmind.geoip2.record.Country" :exclude [:names] :add {:names names-as-kw}]
-     ["com.maxmind.geoip2.record.Traits"]]))
+   {:exclude [:class]}
+   [["com.maxmind.geoip2.model.CityResponse"]
+    ["com.maxmind.geoip2.model.CountryResponse"]
+    ["com.maxmind.geoip2.model.ConnectionTypeResponse"]
+    ["com.maxmind.geoip2.model.IspResponse"]
+    ["com.maxmind.geoip2.model.DomainResponse"]
+    ["com.maxmind.geoip2.record.City" :exclude [:names] :add {:names names-as-kw}]
+    ["com.maxmind.geoip2.record.Continent" :exclude [:names] :add {:names names-as-kw}]
+    ["com.maxmind.geoip2.record.Country" :exclude [:names] :add {:names names-as-kw}]
+    ["com.maxmind.geoip2.record.Subdivision" :exclude [:names] :add {:names names-as-kw}]
+    ["com.maxmind.geoip2.record.RepresentedCountry" :exclude [:names] :add {:names names-as-kw}]
+    ["com.maxmind.geoip2.record.Location"]
+    ["com.maxmind.geoip2.record.MaxMind"]
+    ["com.maxmind.geoip2.record.Postal"]
+    ["com.maxmind.geoip2.record.Traits" :exclude [:network]]]))
+
 
 (defn make-locator
-  "Returns a locator from either a String file path, a File object or an InputStream. This locator is threadsafe. Takes 2 optional arguments:
+  "Returns a locator from either a String file path, a File object or an InputStream. This locator is thread-safe.
+  Takes 2 optional arguments:
  + **:locales** List of recognised locales, for example [:en :fr], used for default :name
  + **:file-mode** Either *:memory-mapped* (default) or *:memory*"
   [f & {:keys [locales file-mode]}]
-  (let [arg (if (instance? java.io.InputStream f) f (io/file f))
+  (let [arg     (if (instance? java.io.InputStream f) f (io/file f))
         builder (DatabaseReader$Builder. arg)]
     (-> builder
         (cond-> locales (.locales (map name locales)))
         (cond-> file-mode (.fileMode (get file-modes file-mode)))
         (.build))))
 
-(defn lookup
-  "Lookup an IP given as a String in the given locator and returns a map with results"
+
+(defn lookup-city
+  "Lookup an `ip` given as a String in the given city `db` and returns a map with results.
+  Scoped to a city level."
   [^GeoIp2Provider db ip]
   (if-let [city (.city db (InetAddress/getByName ip))]
     (g/translate translator city {:lazy? false})))
+
+
+(defn lookup-country
+  "Lookup an `ip` given as a String in the given country `db` and returns a map with results.
+  Scoped to a country level."
+  [^GeoIp2Provider db ip]
+  (if-let [country (.country db (InetAddress/getByName ip))]
+    (g/translate translator country {:lazy? false})))
--- test/com/oscaro/maximator_test.clj
+++ test/com/oscaro/maximator_test.clj
@@ -1,7 +1,21 @@
 (ns com.oscaro.maximator-test
+  "This test requires the *.mmdb files to be present in the test root folder.
+  Please download them from the MaxMind website."
   (:require [clojure.test :refer :all]
-            [com.oscaro.maximator :refer :all]))
+            [bpindelski.maximator :refer :all]
+            [clojure.pprint :as pp]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def locators {:city    (make-locator "test/GeoLite2-City.mmdb" :locales [:en])
+               :country (make-locator "test/GeoLite2-Country.mmdb" :locales [:de :fr])})
+
+
+(deftest basic-tests
+  (testing "city"
+    (let [city (lookup-city (:city locators) "2.21.172.132")]
+      (is (= "Vienna" (get-in city [:city :name])))
+      (is (= "AT" (get-in city [:country :iso-code])))))
+  (testing "country"
+    (let [country (lookup-country (:country locators) "8.8.8.8")]
+      (is (= "US" (get-in country [:country :iso-code])))
+      (pp/pprint country))))
